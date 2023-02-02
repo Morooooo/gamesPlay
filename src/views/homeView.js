97a427e12ab11cec626bd1bbc438a74264cdf06a
Morooooo <102239793+Morooooo@users.noreply.github.com>
2023-02-02 16:38:21 +0200

    Update homeView.js
    minor changes

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -1,50 +1,47 @@
 import {render,html} from '../../node_modules/lit-html/lit-html.js'
+import { getAllGames } from '../api/gamesData.js'
 const main=document.querySelector('#main-content')
-const homeViewTemplate=(user)=>html`<div id="home-page">
+
+const game=(g)=>html`<div class="game">
+<div class="image-wrap">
+    <img src="${g.imageUrl}">
+</div>
+<h3>${g.title}</h3>
+<div class="rating">
+    ${html`<span>☆</span>`}
+</div>
+<div class="data-buttons">
+    <a href="/allGames/${g._id}" class="btn details-btn">Details</a>
+</div>
+</div>`
+const allGames=(games)=>html`<ul>${games.map(g=>game(g))}</ul>
+`
+const homeViewTemplate=(user,list)=>html` <!--Home Page-->
+<section id="welcome-world">
+
+    <div class="welcome-message">
+        <h2>ALL new games are</h2>
+        <h3>Only in GamesPlay</h3>
+    </div>
+    <img src="./images/four_slider_img01.png" alt="hero">
+
+    
+
+      <div id="home-page">
+
 <h1>Latest Games</h1>
-${user?html`<div class="game">
-<div class="image-wrap">
-    <img src="./images/CoverFire.png">
-</div>
-<h3>Cover Fire</h3>
-<div class="rating">
-    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-</div>
-<div class="data-buttons">
-    <a href="#" class="btn details-btn">Details</a>
-</div>
-</div>
-<div class="game">
-<div class="image-wrap">
-    <img src="./images/ZombieLang.png">
-</div>
-<h3>Zombie Lang</h3>
-<div class="rating">
-    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-</div>
-<div class="data-buttons">
-    <a href="#" class="btn details-btn">Details</a>
-</div>
-</div>
-<div class="game">
-<div class="image-wrap">
-    <img src="./images/MineCraft.png">
-</div>
-<h3>MineCraft</h3>
-<div class="rating">
-    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-</div>
-<div class="data-buttons">
-    <a href="#" class="btn details-btn">Details</a>
-</div>
-</div>`:html`<!-- Display paragraph: If there is no games  -->
+${user?allGames(list):html`<!-- Display paragraph: If there is no games  -->
 <p class="no-articles">No games yet</p>`}
 <!-- Display div: with information about every game (if any) -->
-
-
-
-</div>`
-export const homeView=(ctx)=>{
-let template=homeViewTemplate(ctx.user)
+</div>  
+        
+    </div>
+</section>`
+/**/
+export const homeView=async(ctx)=>{
+    let list=await getAllGames()
+    //list.map(x=>console.log(x)) 
+let template=homeViewTemplate(ctx.user,list)
 render(template,main)
+//console.log(ctx.user)
 }
