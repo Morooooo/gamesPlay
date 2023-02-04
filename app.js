806b35944c348c790c51c5a886c3d925b85d6ca6
Morooooo <102239793+Morooooo@users.noreply.github.com>
2023-02-04 17:28:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import { getAllGames } from './src/api/gamesData.js'
 
 import { navBarMiddleware } from './src/middlewares/navBarMiddleWare.js'
 import { verifyUserData } from './src/middlewares/validateUserDataMiddleWare.js'
+import { createView } from './src/views/createView.js'
+//import { commentsView } from './src/views/commentsView.js'
+import { deleteView } from './src/views/deleteGame.js'
 import { detailsView } from './src/views/detailsView.js'
+import { editView } from './src/views/editView.js'
 import { homeView } from './src/views/homeView.js'
 import { loginView } from './src/views/loginView.js'
 import { logoutView } from './src/views/logoutView.js'
@@ -17,4 +21,8 @@ page('/login',loginView)
 page('/logout',logoutView)
 page('/allGames',getAllGames)
 page('/allGames/:params',detailsView)
+page('/allGames/:params/edit',editView)
+page('/allGames/:params/delete',deleteView)
+page('/create',createView)
+//page(`/allGames/:params/comments`,commentsView)
 page.start()
